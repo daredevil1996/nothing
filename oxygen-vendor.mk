54fc3705a1453eb5b5074967fda4bdc55b91f817
Nikit Sharma <nikitsharma92@gmail.com>
2018-03-26 17:01:47 +0530

    oxygen: Hax libsettings to load N libprotobuf

--- oxygen-vendor.mk
+++ oxygen-vendor.mk
@@ -1774,6 +1774,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/oxygen/proprietary/vendor/lib64/libmmosal.so:system/vendor/lib64/libmmosal.so \
     vendor/xiaomi/oxygen/proprietary/vendor/lib64/libnetmgr.so:system/vendor/lib64/libnetmgr.so \
     vendor/xiaomi/oxygen/proprietary/vendor/lib64/libperipheral_client.so:system/vendor/lib64/libperipheral_client.so \
+    vendor/xiaomi/oxygen/proprietary/vendor/lib64/libprotobuf-cpp-S.so:system/vendor/lib64/libprotobuf-cpp-S.so \
     vendor/xiaomi/oxygen/proprietary/vendor/lib64/libqcci_legacy.so:system/vendor/lib64/libqcci_legacy.so \
     vendor/xiaomi/oxygen/proprietary/vendor/lib64/libqcmaputils.so:system/vendor/lib64/libqcmaputils.so \
     vendor/xiaomi/oxygen/proprietary/vendor/lib64/libqdi.so:system/vendor/lib64/libqdi.so \
